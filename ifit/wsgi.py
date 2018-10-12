c849c82a56d461de5475507f06f5088ba4ee26df
Alison Aguiar <alysson.k888@gmailcom>
2018-10-11 23:37:11 -0300

    commit api

--- ifit/wsgi.py
+++ ifit/wsgi.py
@@ -9,16 +9,12 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 
 import os
 
-#from django.core.wsgi import get_wsgi_application
-
-
-#application = get_wsgi_application()
-
-import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+
+application = get_wsgi_application()
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ifit.settings')
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)

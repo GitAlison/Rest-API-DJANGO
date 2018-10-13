fe64249c257f3543ac4c299f2f0cdbc075b55c5f
Alison Aguiar <alysson.k888@gmailcom>
2018-10-13 14:37:23 -0300

    Troca de textos do site

--- ifit/settings.py
+++ ifit/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c#j7%^0&pm=5fzz6jtdd8h#h6zc(nv2upq6ilgeid^539c@4uz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
